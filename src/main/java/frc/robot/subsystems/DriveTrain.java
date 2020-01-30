a84f5f1d2f35f27591dd3664a32122702effc6a2
Fernando-Matias <fxrncndx(FernandoMatias)@users.noreply.github.com>
2020-01-30 04:41:57 -0800

    finished drive train test pending

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -13,6 +13,7 @@ import com.ctre.phoenix.motorcontrol.*;
 
 import frc.robot.utility.*;
 import frc.robot.RobotMap;
+import frc.robot.Constants;
 
 
 
@@ -38,16 +39,58 @@ public static DifferentialDrive mDrive;
 
 public DriveTrain(){
 
+  //Setting both falcons to the default fx script
   mDriveLeftMaster = new DefaultTalonFXDrive(RobotMap.mDriveLeftA_ID);
   mDriveLeftB = new DefaultTalonFXDrive(RobotMap.mDriveLeftA_ID);
 
   mDriveRightMaster = new DefaultTalonFXDrive(RobotMap.mDriveRightA_ID);
   mDriveRightB = new DefaultTalonFXDrive(RobotMap.mDriveLeftB_ID);
 
+  //enabling follower mode for the other  two motors
   mDriveLeftB.set(ControlMode.Follower,RobotMap.mDriveLeftA_ID);
-  
+
   mDriveRightB.set(ControlMode.Follower,RobotMap.mDriveRightA_ID);
 
+  //creating the differential drive and disabling the saftey
+  mDrive = new DifferentialDrive(mDriveLeftMaster, mDriveRightMaster);
+  mDrive.setSafetyEnabled(false);
+}
+//setting specifications for falcons
+
+public void setCoast() {
+  mDriveLeftMaster.setNeutralMode(NeutralMode.Coast);
+  mDriveLeftB.setNeutralMode(NeutralMode.Coast);
+  mDriveRightMaster.setNeutralMode(NeutralMode.Coast);
+  mDriveRightB.setNeutralMode(NeutralMode.Coast);
+}
+public void setBrake() {
+  mDriveLeftMaster.setNeutralMode(NeutralMode.Brake);
+  mDriveLeftB.setNeutralMode(NeutralMode.Brake);
+  mDriveRightMaster.setNeutralMode(NeutralMode.Brake);
+  mDriveRightB.setNeutralMode(NeutralMode.Brake);
+}
+
+public void EnableVoltComp() {
+  mDriveLeftMaster.enableVoltageCompensation(true);
+  mDriveLeftB.enableVoltageCompensation(true);
+  mDriveRightMaster.enableVoltageCompensation(true);
+  mDriveRightB.enableVoltageCompensation(true);
+}
+public void DisableVoltComp() {
+  mDriveLeftMaster.enableVoltageCompensation(false);
+  mDriveLeftB.enableVoltageCompensation(false);
+  mDriveRightMaster.enableVoltageCompensation(false);
+  mDriveRightB.enableVoltageCompensation(false);
+}
+public void StopDrivetrain() {
+  mDriveLeftMaster.set(ControlMode.PercentOutput, 0.0);
+  mDriveRightMaster.set(ControlMode.PercentOutput, 0.0);
+}
+
+//turning algirithm
+public void Curvature(double ThrottleAxis, double TurnAxis) {
+  TurnRateCurved = (Constants.kTurnrateCurve*Math.pow(TurnAxis,3)+(1-Constants.kTurnrateCurve)*TurnAxis*Constants.kTurnrateLimit);
+  mDrive.curvatureDrive(ThrottleAxis, TurnRateCurved, true);
 }
 
   @Override
--- src/main/java/frc/robot/utility/DefaultTalonFXDrive.java
+++ src/main/java/frc/robot/utility/DefaultTalonFXDrive.java
@@ -10,7 +10,6 @@ package frc.robot.utility;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 /**
  * Author Fernando Matias
